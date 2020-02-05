db16ecaf83360285dccee3e9f21a5a919d24262d
amir hossein <a.mehrabi.70@gmail.com>
2020-02-05 23:37:32 +0330

    added data project => EF

---
+++ FarnahadManufacturing.Data/FarnahadManufacturingDbContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FarnahadManufacturing.Data
+{
+    public class FarnahadManufacturingDbContext : DbContext
+    {
+        public FarnahadManufacturingDbContext() : base("FarnahadManufacturingConnectionString")
+        {
+        }
+
+    }
+}

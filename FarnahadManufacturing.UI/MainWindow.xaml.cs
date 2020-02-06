6ba047b0de681e53d1f49dd790b4215d0a7617c8
amir hossein <a.mehrabi.70@gmail.com>
2020-02-07 01:59:19 +0330

    UI main layout added

--- FarnahadManufacturing.UI/MainWindow.xaml.cs
+++ FarnahadManufacturing.UI/MainWindow.xaml.cs
@@ -12,6 +12,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using DevExpress.Xpf.Docking;
+using FarnahadManufacturing.UI.UserControls;
+using FarnahadManufacturing.UI.UserControls.BaseConfiguration;
 
 namespace FarnahadManufacturing.UI
 {
@@ -23,6 +26,39 @@ namespace FarnahadManufacturing.UI
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Country_OnMouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            OpenUserControlInNewTab<UcCountry>("کشور ها");
+        }
+
+        private void City_OnMouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            OpenUserControlInNewTab<UcCity>("شهر ها");
+        }
+
+        private void OpenUserControlInNewTab<T>(string tabHeader) where T : UserControl
+        {
+            if (!UserControlIsAlreadyOpen<T>())
+            {
+                var panel = new DocumentPanel();
+                panel.TabCaption = tabHeader;
+                panel.AllowClose = true;
+                panel.Content = Activator.CreateInstance<T>();
+                MyDocumentGroup.Items.Add(panel);
+                panel.IsActive = true;
+            }
+        }
+
+        private bool UserControlIsAlreadyOpen<T>() where T : UserControl
+        {
+            foreach (var item in MyDocumentGroup.Items)
+            {
+                if (item is DocumentPanel documentPanel && documentPanel.Content is T)
+                    return true;
+            }
+            return false;
         }
     }
 }
---
+++ FarnahadManufacturing.UI/UserControls/BaseConfiguration/UcCountry.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace FarnahadManufacturing.UI.UserControls.BaseConfiguration
+{
+    /// <summary>
+    /// Interaction logic for UcCountry.xaml
+    /// </summary>
+    public partial class UcCountry : UserControl
+    {
+        public UcCountry()
+        {
+            InitializeComponent();
+        }
+    }
+}

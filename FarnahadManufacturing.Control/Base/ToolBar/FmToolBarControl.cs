88df8d903caace2088945c6ce27e531ce1d00580
amir hossein <a.mehrabi.70@gmail.com>
2020-02-24 22:36:44 +0330

    moved toolbar classes into its folder

--- FarnahadManufacturing.Control/Base/ToolBar/FmToolBarControl.cs
+++ FarnahadManufacturing.Control/Base/ToolBar/FmToolBarControl.cs
@@ -1,6 +1,6 @@
 ﻿using DevExpress.Xpf.Bars;
 
-namespace FarnahadManufacturing.Control.Base.MainPage
+namespace FarnahadManufacturing.Control.Base.ToolBar
 {
     public class FmToolBarControl : ToolBarControl
     {

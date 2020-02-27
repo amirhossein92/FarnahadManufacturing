e069861da62068f740c20f1f8ac8e77f715f0c58
amir hossein <a.mehrabi.70@gmail.com>
2020-02-27 20:39:07 +0330

    added grid columns for spin edit + email text edit

---
+++ FarnahadManufacturing.Control/Base/GridControl/Columns/FmIntSpinEditGridColumn.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DevExpress.Xpf.Editors.Settings;
+
+namespace FarnahadManufacturing.Control.Base.GridControl.Columns
+{
+    public class FmIntSpinEditGridColumn : FmSpinEditGridColumn
+    {
+        public FmIntSpinEditGridColumn()
+        {
+            EditSettings = new SpinEditSettings{Mask = "N0"};
+        }
+    }
+}
---
+++ FarnahadManufacturing.Control/Base/Input/FmEmailTextEdit.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DevExpress.Xpf.Editors;
+
+namespace FarnahadManufacturing.Control.Base.Input
+{
+    public class FmEmailTextEdit : FmTextEdit
+    {
+        public FmEmailTextEdit()
+        {
+            MaskType = MaskType.RegEx;
+            Mask = @"[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}";
+            MaskUseAsDisplayFormat = true;
+        }
+    }
+}
